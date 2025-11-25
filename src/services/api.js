94e6f0bce96d5f5665301b0c80d3ab14777d31c3
rashi <rashig2344@gmail.com>
2025-11-25 12:47:00 +0530

    vhg

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://skillconnect-backend-u3a9.onrender.com/api",
-  withCredentials: true,  // 🔥 MUST BE TRUE
+  withCredentials: true,  
 });
 
 export default api;
